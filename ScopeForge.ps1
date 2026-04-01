439e8ef308c59444c7f606c339d15a015bcf1d97
Z3PHIRE <anthonybellini94@gmail.com>
2026-04-01 17:29:17 +0200

    V.0.14.12

--- ScopeForge.ps1
+++ ScopeForge.ps1
@@ -1424,6 +1424,22 @@ function Save-ScopeForgeTriageState {
         [AllowEmptyCollection()][string[]]$SeenReviewKeys = @()
     )
 
+    function Get-ScopeForgeStatePropertyValue {
+        param(
+            [AllowNull()][object]$InputObject,
+            [Parameter(Mandatory)][string]$PropertyName,
+            $DefaultValue = $null
+        )
+
+        if ($null -eq $InputObject) { return $DefaultValue }
+
+        $property = $InputObject.PSObject.Properties[$PropertyName]
+        if ($null -eq $property) { return $DefaultValue }
+        if ($null -eq $property.Value) { return $DefaultValue }
+
+        return $property.Value
+    }
+
     $normalizedState = $null
     try {
         if ($null -ne $State) {
@@ -1434,101 +1450,59 @@ function Save-ScopeForgeTriageState {
     }
 
     $programName = 'default-program'
-    if (
-        $null -ne $normalizedState -and
-        $null -ne $normalizedState.PSObject.Properties['ProgramName'] -and
-        -not [string]::IsNullOrWhiteSpace([string]$normalizedState.ProgramName)
-    ) {
-        $programName = [string]$normalizedState.ProgramName
-    }
-    elseif (
-        $null -ne $State -and
-        $null -ne $State.PSObject.Properties['ProgramName'] -and
-        -not [string]::IsNullOrWhiteSpace([string]$State.ProgramName)
-    ) {
-        $programName = [string]$State.ProgramName
+    $normalizedProgramName = [string](Get-ScopeForgeStatePropertyValue -InputObject $normalizedState -PropertyName 'ProgramName' -DefaultValue '')
+    $rawProgramName = [string](Get-ScopeForgeStatePropertyValue -InputObject $State -PropertyName 'ProgramName' -DefaultValue '')
+
+    if (-not [string]::IsNullOrWhiteSpace($normalizedProgramName)) {
+        $programName = $normalizedProgramName
+    } elseif (-not [string]::IsNullOrWhiteSpace($rawProgramName)) {
+        $programName = $rawProgramName
     }
 
-    $path = $null
-    if (
-        $null -ne $normalizedState -and
-        $null -ne $normalizedState.PSObject.Properties['Path'] -and
-        -not [string]::IsNullOrWhiteSpace([string]$normalizedState.Path)
-    ) {
-        $path = [string]$normalizedState.Path
+    $path = [string](Get-ScopeForgeStatePropertyValue -InputObject $normalizedState -PropertyName 'Path' -DefaultValue '')
+    if ([string]::IsNullOrWhiteSpace($path)) {
+        $path = [string](Get-ScopeForgeStatePropertyValue -InputObject $State -PropertyName 'Path' -DefaultValue '')
     }
-    elseif (
-        $null -ne $State -and
-        $null -ne $State.PSObject.Properties['Path'] -and
-        -not [string]::IsNullOrWhiteSpace([string]$State.Path)
-    ) {
-        $path = [string]$State.Path
-    }
-    else {
+    if ([string]::IsNullOrWhiteSpace($path)) {
         $path = Join-Path (Get-ScopeForgeStateRoot -ProgramName $programName) 'triage-state.json'
     }
 
-    $ignoreValues = @()
-    if (
-        $null -ne $normalizedState -and
-        $null -ne $normalizedState.PSObject.Properties['IgnoreKeys'] -and
-        $null -ne $normalizedState.IgnoreKeys
-    ) {
-        $ignoreValues = @(ConvertTo-ArrayOrEmpty -Data $normalizedState.IgnoreKeys | ForEach-Object { [string]$_ })
-    }
+    $ignoreValues = @(
+        ConvertTo-ArrayOrEmpty -Data (Get-ScopeForgeStatePropertyValue -InputObject $normalizedState -PropertyName 'IgnoreKeys' -DefaultValue @()) |
+        ForEach-Object { [string]$_ } |
+        Where-Object { -not [string]::IsNullOrWhiteSpace($_) }
+    )
 
-    $falsePositiveValues = @()
-    if (
-        $null -ne $normalizedState -and
-        $null -ne $normalizedState.PSObject.Properties['FalsePositiveKeys'] -and
-        $null -ne $normalizedState.FalsePositiveKeys
-    ) {
-        $falsePositiveValues = @(ConvertTo-ArrayOrEmpty -Data $normalizedState.FalsePositiveKeys | ForEach-Object { [string]$_ })
-    }
+    $falsePositiveValues = @(
+        ConvertTo-ArrayOrEmpty -Data (Get-ScopeForgeStatePropertyValue -InputObject $normalizedState -PropertyName 'FalsePositiveKeys' -DefaultValue @()) |
+        ForEach-Object { [string]$_ } |
+        Where-Object { -not [string]::IsNullOrWhiteSpace($_) }
+    )
 
-    $validatedValues = @()
-    if (
-        $null -ne $normalizedState -and
-        $null -ne $normalizedState.PSObject.Properties['ValidatedKeys'] -and
-        $null -ne $normalizedState.ValidatedKeys
-    ) {
-        $validatedValues = @(ConvertTo-ArrayOrEmpty -Data $normalizedState.ValidatedKeys | ForEach-Object { [string]$_ })
-    }
+    $validatedValues = @(
+        ConvertTo-ArrayOrEmpty -Data (Get-ScopeForgeStatePropertyValue -InputObject $normalizedState -PropertyName 'ValidatedKeys' -DefaultValue @()) |
+        ForEach-Object { [string]$_ } |
+        Where-Object { -not [string]::IsNullOrWhiteSpace($_) }
+    )
 
-    $seenValues = @()
-    if (
-        $null -ne $normalizedState -and
-        $null -ne $normalizedState.PSObject.Properties['SeenKeys'] -and
-        $null -ne $normalizedState.SeenKeys
-    ) {
-        $seenValues = @(ConvertTo-ArrayOrEmpty -Data $normalizedState.SeenKeys | ForEach-Object { [string]$_ })
-    }
+    $seenValues = @(
+        ConvertTo-ArrayOrEmpty -Data (Get-ScopeForgeStatePropertyValue -InputObject $normalizedState -PropertyName 'SeenKeys' -DefaultValue @()) |
+        ForEach-Object { [string]$_ } |
+        Where-Object { -not [string]::IsNullOrWhiteSpace($_) }
+    )
+
+    $seenReviewValues = @(
+        ConvertTo-ArrayOrEmpty -Data $SeenReviewKeys |
+        ForEach-Object { [string]$_ } |
+        Where-Object { -not [string]::IsNullOrWhiteSpace($_) }
+    )
 
     $ignoreKeys = New-ScopeForgeStringSet -Values $ignoreValues
     $falsePositiveKeys = New-ScopeForgeStringSet -Values $falsePositiveValues
     $validatedKeys = New-ScopeForgeStringSet -Values $validatedValues
-    $seenKeys = New-ScopeForgeStringSet -Values $seenValues
+    $seenKeys = New-ScopeForgeStringSet -Values @($seenValues + $seenReviewValues)
 
-    if ($null -eq $ignoreKeys) { $ignoreKeys = New-ScopeForgeStringSet }
-    if ($null -eq $falsePositiveKeys) { $falsePositiveKeys = New-ScopeForgeStringSet }
-    if ($null -eq $validatedKeys) { $validatedKeys = New-ScopeForgeStringSet }
-    if ($null -eq $seenKeys) { $seenKeys = New-ScopeForgeStringSet }
-
-    foreach ($key in @(ConvertTo-ArrayOrEmpty -Data $SeenReviewKeys)) {
-        $reviewKey = [string]$key
-        if (-not [string]::IsNullOrWhiteSpace($reviewKey)) {
-            [void]$seenKeys.Add($reviewKey)
-        }
-    }
-
-    $reviewNotes = @{}
-    if (
-        $null -ne $normalizedState -and
-        $null -ne $normalizedState.PSObject.Properties['ReviewNotes'] -and
-        $null -ne $normalizedState.ReviewNotes
-    ) {
-        $reviewNotes = $normalizedState.ReviewNotes
-    }
+    $reviewNotes = Get-ScopeForgeStatePropertyValue -InputObject $normalizedState -PropertyName 'ReviewNotes' -DefaultValue @{}
 
     $parentDir = Split-Path -Parent $path
     if (
